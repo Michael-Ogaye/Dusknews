ddc7c957022512c9fd31695c009a5746acce342b
Michael Ogaye <hogaeymic@gmail.com>
2022-04-30 12:09:35 +0300

    created the development server and base,html, index.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,11 @@
 from flask import render_template,request,redirect,url_for
+from . import main
+
+
+# index route
+@main.route('/')
+def index():
+
+    title="highlights"
+
+    return render_template('index.html', title=title)
